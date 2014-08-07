0f1a878a41e9426f0ad7063a405f8c193e2b9a6e
Guoqing Zheng <gzheng@cs.cmu.edu>
2014-08-06 20:28:54 -0400

    project configuration file

---
+++ configuration.py
@@ -0,0 +1,16 @@
+# Put constants and configurations here
+
+WORD_VECTOR_MODELS = [ '/bos/tmp17/gzheng/clueweb09b-new-100.bin',
+                       '/bos/tmp17/gzheng/clueweb09b-new-300.bin',
+                       '/bos/tmp17/gzheng/clueweb09b-new-500.bin',
+                       '/bos/usr0/gzheng/TermRecall/src/word2vec/GoogleNews-vectors-negative300.bin']
+
+SEARCH_ENGINES_FILE = 'indices.txt'
+
+RETRIEVAL_MODELS = ['LM', 'BM25']
+
+RETRIEVAL_TOP_DOCS = [1000]
+
+INDRIRUNQUERY = '/bos/usr0/gzheng/.local/bin/IndriRunQuery'
+
+
