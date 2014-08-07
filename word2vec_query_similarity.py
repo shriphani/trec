7b830788741f6072f1184a4f7ea8afafd0d3a2e2
Guoqing Zheng <gzheng@cs.cmu.edu>
2014-08-06 20:55:51 -0400

    word2vec similarity for measuring two queries

---
+++ word2vec_query_similarity.py
@@ -0,0 +1,19 @@
+import gensim
+from collections import defaultdict
+
+class WordVecSimilariy(object):
+    def __init__(self, model_file):
+        print 'Loading model %s ...' % model_file
+        self.model = gensim.models.Word2Vec.load_word2vec_format(model_file, binary=True)
+        print 'Loading model done.'
+
+
+    # q is a list of processed terms
+    def similarity(self, q1, q2):
+
+        try:
+            rep = model[term]
+        except KeyError:
+            print 'KEY ERROR FOR:', ' ', 'abandoned.'
+            
+        return 1.0
