12d1037eb31afb1557a4954ea2743e280488e6c0
Guoqing Zheng <gzheng@cs.cmu.edu>
2014-08-07 15:21:28 -0400

    developing...

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
 import sys
 import configuration
+import word2vec_query_similarityo
 
 # main entry for all three tasks here
+
+def read_queries(query_file):
+    queries = []
+    with open(query_file, 'r') as qf:
+        for line in qf:
+            parts = lint.strip().split(':')
+            qid = int(parts[0])
+            query = parts[1].strip()
+
+        queries.append((qid, query))
+
+    return queries
+
+def read_engines(engine_file):
+    engines = []
+    with open(engine_file, 'r') as ef:
+        for line in ef:
+            engines.append(line.strip())
+
+    return engines
+
+if __name__ == '__main__':
+    
+    wv_model = WordVecSimilarity(WORD_VECTOR_MODELS[-1])
+    
+    test_queries = read_queries(TEST_QUERY_FILE)
+    sample_queries = read_queries(SAMPLE_QUERY_FILE)
+
+    engines = read_engines(SEARCH_ENGINES_FILE)
+    
+    
+    
+    
--- word2vec_query_similarity.py
+++ word2vec_query_similarity.py
@@ -1,12 +1,11 @@
 import gensim
 from collections import defaultdict
 
-class WordVecSimilariy(object):
+class WordVecSimilarity(object):
     def __init__(self, model_file):
         print 'Loading model %s ...' % model_file
         self.model = gensim.models.Word2Vec.load_word2vec_format(model_file, binary=True)
         print 'Loading model done.'
-
 
     # q is a list of processed terms
     def similarity(self, q1, q2):
